d9fb58f517e2263b32eceb87dc9f432b47fc8a03
stevenadema <stevenadema@gmail.com>
2021-01-05 21:41:13 -0500

    blunders aggregated

--- main.py
+++ main.py
@@ -2,128 +2,34 @@ import chess
 import chess.pgn
 import chess.engine
 import pandas as pd
-from dateutil.relativedelta import relativedelta
-import io
-import requests
-import json
-from pgn_parser import pgn, parser
-from datetime import date
-from read_pgn import PGNReader
 pd.set_option('display.max_rows', 15)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
-pd.set_option('display.max_colwidth', 20)
+pd.set_option('display.max_colwidth', 100)
 
 
 def main():
-    pgn = PGNReader('stevenadema', 2)
-    pgn.filter_time_control('600')
-    print(pgn.df)
-    # df.to_csv('df.csv', index=False, sep='|')
+    # pgn = PGNReader('stevenadema', 2)
+    # pgn.filter_time_control('600')
+    df = pd.read_csv('df.csv', sep="|")
 
-    exit()
-    game_moves_df = get_move_scores(df, username, enginepath, eval_time, limit)
-    print(game_moves_df)
-    game_moves_df.to_csv('game_moves_df.csv', index=False, sep='|')
+    print(df.shape)
+    df = df[df['difs'] > 50]
+    df = df.loc[df['time_control'] == 600]
+    df = df.reset_index()
+    print(df.shape)
+    # df = df.groupby(['fen_y']).count()
+    df = df.sort_values(by='index', ascending=False)
 
-    bad_moves = game_moves_df[game_moves_df['difs'] > 40]
-    print(bad_moves[['mv', 'mv_score', 'bmv', 'bmv_score', 'difs']])
+    fen = df.iloc[2]['fen_y']
+    print(df.iloc[2])
+    board = chess.Board(fen)
+    engine = chess.engine.SimpleEngine.popen_uci('C:/Users/Steven/Downloads/stockfish_12_win_x64_bmi2/stockfish_20090216_x64_bmi2.exe')
+    info = engine.analyse(board, chess.engine.Limit(depth=20))
+    print(board)
+    print(info)
     
 
-def get_move_scores(df, username, enginepath, eval_time, limit):
-    fen, mv, mv_score, bmv, bmv_score, difs = [], [], [], [], [], []
-
-    for index, row in df.iterrows():
-        g = df.iloc[index]['pgn']
-        print('analyzing game ', index)
-
-        user_color = df.iloc[index]['user_color']
-
-        pgn = io.StringIO(g)
-        game = chess.pgn.read_game(pgn)
-
-        # setup board and engine
-        board = chess.Board()
-        engine = chess.engine.SimpleEngine.popen_uci(enginepath)
-        i = 0
-
-        for move in game.mainline_moves():
-            # Only analyse user moves
-            if ((i % 2 == 0 and user_color == 'black') or (i % 2 == 1 and user_color == 'white')):
-                board.push(move)
-                i += 1
-                continue 
-
-            # Get move, score and difference of user move and best move 
-            mymove_info = engine.analyse(board, chess.engine.Limit(time=eval_time))
-            best_move = mymove_info['pv'][0]
-            board.push(move)
-            mymove_info = engine.analyse(board, chess.engine.Limit(time=eval_time))
-            mymove_score = get_move_score(mymove_info, user_color)
-            board.pop()
-            board.push(best_move)
-            bestmove_info = engine.analyse(board, chess.engine.Limit(time=eval_time))
-            bestmove_score = get_move_score(bestmove_info, user_color)
-            dif = bestmove_score - mymove_score
-            board.pop()
-            board.push(move)
-
-            # get fens, labels, povscores, difs
-            fen.append(str(board.fen()))
-            mv.append(str(move))
-            mv_score.append(mymove_score)
-            bmv.append(str(best_move))
-            bmv_score.append(bestmove_score)
-            difs.append(dif)
-
-            if i > limit:
-                break
-            i += 1
-            
-        engine.quit()
-
-    game_moves_df = pd.DataFrame(
-        {'fen': fen,
-         'mv': mv,
-         'mv_score': mv_score,
-         'bmv': bmv,
-         'bmv_score': bmv_score,
-         'difs': difs
-        })
-    return game_moves_df
-
-def get_move_score(board_info, color, mate=1500):
-    if color == 'white':
-        if board_info['score'].is_mate():
-            m = board_info['score'].white().score(mate_score=mate)
-        else: 
-            m = int(format(board_info['score'].white().score()))
-    elif color == 'black': 
-        if board_info['score'].is_mate():
-            m = board_info['score'].black().score(mate_score=mate)
-        else: 
-            m = int(format(board_info['score'].black().score()))
-    else:
-        print('Invalid color perspective chosen')
-
-    return m        
-
-def get_games(user, lookback, df):
-    dt = date.today()
-
-    while lookback > -1:
-        dt_f = dt - relativedelta(months=lookback)
-        dt_f = dt_f.strftime('%Y/%m')
-        response = requests.get(
-            r'https://api.chess.com/pub/player/' + user + '/games/' + dt_f)
-        j = json.loads(response.content.decode('utf-8'))
-        # with open('response.json', 'w') as json_file:
-        #     json.dump(j, json_file)
-        df_new = pd.DataFrame(j['games'])
-        df = df.append(df_new, sort=True)
-        lookback -= 1
-
-    return df
 
 if __name__ == "__main__":
     main()
